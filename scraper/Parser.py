92e271724ec7d6e77d18ec94d01ab07d82c83e9f
Ananiya Tesfahun <ananiyat24@gmail.com>
2024-04-11 15:16:32 +0300

    chore: parser ONLY handles parsing

--- scraper/Parser.py
+++ scraper/Parser.py
@@ -1,6 +1,5 @@
 import sys
 from pathlib import Path
-import json
 
 sys.path.append(str(Path(__file__).parent.parent))
 
@@ -9,17 +8,8 @@ from bs4 import BeautifulSoup
 from typing import *
 import time
 import asyncio
-from models import BaseModel, Circuit, RaceEvent, Result
-from models.EventType import EventType
-from models.Race import Race
+from models import Race
 from dataclasses import asdict
-
-async def print_when_done(function):
-    async def wrapper():
-        await function()
-        print("Done")
-        
-    return wrapper
 
 class Parser:
     BASE_URL = "https://www.formula1.com"
@@ -73,7 +63,7 @@ class Parser:
         return raceUrls
     
     
-    async def getCircuit(self, raceUrl: str) -> Circuit:
+    async def getCircuit(self, raceUrl: str) -> Dict[str, str]:
         splitUrl = raceUrl.split(".")
         splitUrl.pop()
         url = ".".join(splitUrl) + "/Circuit.html"
@@ -92,69 +82,73 @@ class Parser:
             contents = [content for content in stat.stripped_strings]
             circuit[self.__propMapper(contents[0], propMap)] = " ".join(contents[1:])
         
-        return Circuit.fromDict(**circuit)
+        return circuit
     
     
-    async def getEvent(self, raceId: str, containerDiv: BeautifulSoup, completed: bool = False, getReults: bool = True) -> RaceEvent:
+    def __parseEvent(self, raceId: str, containerDiv: BeautifulSoup) -> dict[str, str]:
         title = containerDiv.find('p', class_='f1-timetable--title')
         time = containerDiv.parent['data-start-time'].split("T")
         gmtOff = containerDiv.parent['data-gmt-offset']
         
-        event = RaceEvent(
-            raceId=raceId,
-            title=title.string,
-            date=time[0],
-            time=time[1],
-            gmtOffset=gmtOff,
-            type_=EventType.getType(title.string)
-        )
+        event = {
+            "raceId": raceId,
+            "title": title.string,
+            "date": time[0],
+            "time": time[1],
+            "gmtOffset": gmtOff,
+            "resultLink": None
+        }
         
-        if completed and getReults:
-            resultLink = containerDiv.find('a')['href']
-            results = await self.getEventResults(resultLink, event.id)
-            event.addResults(results)
+        resultLinkA = containerDiv.find('a')
+        
+        if resultLinkA:
+            resultLink = resultLinkA['href']
+            event["resultLink"] = resultLink
             
         return event
     
-    
-    async def getEventResults(self, url : str, eventId: str) -> List[Result]:
-        resultArr = []
-        soup = await self.__getSoup(url)
+    def __parseTable(self, table: BeautifulSoup, propMap: dict[str, str] = {}, initial: dict[str, str] = {}) -> List[dict[str, str]]:
+        infos = [ele.lower() for ele in table.thead.tr.stripped_strings]
         
-        title = soup.find('h1', class_='ResultsArchiveTitle').string.split("-")[-1].strip()
-        type_ = EventType.getType(title)
-        resultTable = soup.find('table', class_='resultsarchive-table')
-        
-        infos = [ele for ele in resultTable.thead.tr.stripped_strings]
-        
-        propMap = { 
-            "pos": "position", "driver": "driverName", "car": "constructorName", "no": "driverId",
-            "time/retired": "time", "pts": "points"
-        }
-        for child in resultTable.tbody.children:
+        tableData: List[dict[str, str]] = []
+        for child in table.tbody.children:
             if child == "\n":
                 continue
             
-            data = []
+            rowData = []
             for ch in child.children:
                 joined = " ".join(ch.stripped_strings)
                 if joined:
-                    data.append(joined)
+                    rowData.append(joined)
             
-            while len(data) < len(infos):
-                end = data.pop()
-                data.append("")
-                data.append(end)
+            while len(rowData) < len(infos):
+                end = rowData.pop()
+                rowData.append("")
+                rowData.append(end)
                 
-            result = { "type_": type_, "eventId": eventId }
-            for ind, res in enumerate(data):
-                result[self.__propMapper(infos[ind].lower(), propMap)] = res
+            row = initial.copy()
+            for ind, res in enumerate(rowData):
+                row[self.__propMapper(infos[ind].lower(), propMap)] = res
                 
-            result["driverName"] = " ".join(result["driverName"].split()[:-1])
-                
-            resultArr.append(Result.fromDict(**result))
+            tableData.append(row)
+            
+        return tableData
         
-        return resultArr
+    
+    async def getEventResults(self, url : str, eventId: str) -> List[Dict[str, str]]:
+        soup = await self.__getSoup(url)
+        
+        title = soup.find('h1', class_='ResultsArchiveTitle').string.split("-")[-1].strip()
+        resultTable = soup.find('table', class_='resultsarchive-table')
+        
+        propMap = { 
+            "pos": "position", "driver": "driverName", "car": "constructorName", "no": "driverNumber",
+            "time/retired": "time", "pts": "points"
+        }
+        
+        results = self.__parseTable(resultTable, propMap, initial={"eventId": eventId, "eventTitle": title})
+        
+        return results
     
     def __isTrackMap(self, alt: str) -> bool:
         return alt and ('carbon.png' in alt or 'carbon_original.png' in alt.lower())
@@ -165,29 +159,57 @@ class Parser:
         return int(splitUrl[splitUrl.index("racing") + 1])
     
     
-    async def getRace(self, url: str, round_: int, getResults: bool = True) -> Race:
+    async def getRace(self, url: str, round_: int) -> Dict[str, Any]:
         soup = await self.__getSoup(url)
         
         year = self.__getYear(url)
         raceLocation = soup.find('h1', class_='race-location').contents[0]
         raceName = soup.find('h2', class_='f1--s').string
         trackMapImg = soup.find('img', alt=self.__isTrackMap)['data-src']
-        
-        race = Race(year, round_, raceName, raceLocation, trackMapImg)
+        raceId = Race.formatRaceId(year, round_)
 
         upcoming = soup.find_all('div', class_='upcoming')
         completed = soup.find_all('div', class_='completed')
-            
-        tasks = [self.getEvent(race.id, eventDiv) for eventDiv in upcoming]
-        tasks += [self.getEvent(race.id, eventDiv, completed=True, getReults=getResults) for eventDiv in completed]
-        tasks.append(self.getCircuit(url))
         
-        events = await asyncio.gather(*tasks)
+        events = [self.__parseEvent(raceId ,eventDiv) for eventDiv in upcoming + completed]
+        circuit = await self.getCircuit(url)
         
-        race.addEvents(events[:-1])
-        
-        circuit = events[-1]
-        race.setCircuit(circuit)
+        race = {
+            "year": year,
+            "round_": round_,
+            "name": raceName,
+            "location": raceLocation,
+            "trackMap": trackMapImg,
+            "circuit": circuit,
+            "events": events
+        }
         
         return race
     
+    
+    async def __getStandings(self, url: str, propMap: dict[str, str] = {}) -> List[dict[str, str]]:
+        soup = await self.__getSoup(url)
+        
+        table = soup.find('table', class_='resultsarchive-table')
+      
+        standings = self.__parseTable(table, propMap)
+        return standings
+    
+    
+    async def getDriverStandings(self, year: int) -> List[Dict[str, str]]:
+        url = self.DRIVERS_STANDINGS_URL.format(year=year)
+        propMap = { "pos": "position", "driver": "driverName", "car": "constructorName", "pts": "points" }
+        
+        driverStandings = await self.__getStandings(url, propMap)
+        
+        return driverStandings
+    
+    
+    async def getConstructorStandings(self, year: int) -> List[Dict[str, str]]:
+        url = self.CONSTRUCTORS_STANDINGS_URL.format(year=year)
+        propMap = { "pos": "position", "team": "constructorName", "pts": "points" }
+        
+        constructorStandings = await self.__getStandings(url, propMap)
+        
+        return constructorStandings
+        
