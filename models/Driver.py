6f234d3f498e1d7279738cf3bd91f266fc5c2384
Ananiya Tesfahun <ananiyat24@gmail.com>
2024-04-27 23:37:38 +0300

    feat - driver id scheme

--- models/Driver.py
+++ models/Driver.py
@@ -1,12 +1,25 @@
 from dataclasses import dataclass
 from .BaseModel import BaseModel
-from .Constructor import Constructor
+from functools import lru_cache
+import json
 
 @dataclass
 class Driver(BaseModel):
-    id: str
     name: str
     shortName: str
     nationality: str
     constructorId: str
-    constructor: Constructor
+    id_: str = None
+    
+    def __post_init__(self):
+        self.id_ = self.id_ if self.id_ else self.getDriverId(self.name)
+    
+
+    @staticmethod
+    @lru_cache
+    def getDriverId(name: str) -> str:
+        with open("constants/drivers.json", "r") as f:
+            drivers = json.load(f)
+            for driver in drivers:
+                if driver["name"] == name:
+                    return driver["id"]
