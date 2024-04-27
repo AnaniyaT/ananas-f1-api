8f4316efe7c8f65f4dc06e4a4ca60b0e4ce5ce1a
Ananiya Tesfahun <ananiyat24@gmail.com>
2024-04-27 23:40:49 +0300

    feat - finalize circuit model class

--- models/Circuit.py
+++ models/Circuit.py
@@ -1,5 +1,12 @@
+import sys
+from pathlib import Path
+sys.path.append(str(Path(__file__).parent.parent))
+
 from dataclasses import dataclass
+import json
 from .BaseModel import BaseModel
+from utils import Utils
+
 
 @dataclass
 class Circuit(BaseModel):
@@ -8,17 +15,33 @@ class Circuit(BaseModel):
     length: float
     raceDistance: float
     lapRecord: str
-    firstGrandPrix: str
-    id: str = None
+    firstGrandPrix: int
+    id_: str = None
     
     def __str__(self):
         return self.name
     
     def __post_init__(self):
-        self.id = self.name
+        self.id_ = self.getCircuitId(self.name)
     
     @staticmethod
     def fromDict(**kwargs):
         return Circuit(**kwargs)
+    
+    @staticmethod
+    def getCircuitId(name: str):
+        with open("constants/circuits.json") as f:
+            circuits = json.load(f)
+            bestMatch = None
+            bestMatchDistance = 20
+            
+            for circuit in circuits:
+                distance = Utils.editDistance(circuit["name"], name)
+                if distance < bestMatchDistance:
+                    bestMatch = circuit
+                    bestMatchDistance = distance
+                    
+        return bestMatch["id"] if bestMatch else None
+            
 
     
